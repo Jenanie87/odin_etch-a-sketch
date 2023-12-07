72fa1ea64d9ce1ab81b78a6787d85bd13790fbd0
Jenanie87 <107427021+Jenanie87@users.noreply.github.com>
2023-12-07 17:33:27 +0100

    Add button and function with color

--- index.js
+++ index.js
@@ -1,28 +1,92 @@
 let container = document.querySelector(".container");
+let mainPart = document.querySelector(".main-part");
+
+let eraseButton = document.querySelector(".btnErase");
 
 function numbersSquare() {
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            const columnDiv = document.createElement("div");
-            container.appendChild(columnDiv);
-            columnDiv.classList.add("squareColumn");
-        }
-        const rowDiv = document.createElement("div");
-        columnDiv.appendChild(rowDiv);
-        columnDiv.classList.add("squareRow");
-/*         let newLine = document.createElement("br");
-        container.appendChild(newLine);
-        newLine.classList.add("br"); */
+    n = slider.value;
+    for (let j = 0; j < n; j++) {
+        const columnDiv = document.createElement("div");
+        columnDiv.classList.add("squareColumn");
+        container.appendChild(columnDiv);
+        for (let i = 0; i < n; i++) {
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("squareRow");
+            columnDiv.appendChild(rowDiv);   
+            
+            // Change the color 
 
+            rowDiv.addEventListener("mouseover", () => {
+                rowDiv.style.background = "grey";
+                setTimeout(() => {
+                    rowDiv.style.backgroundColor = "#c0c0c0";    
+                }, 700);
+                setTimeout(() => {
+                    rowDiv.style.backgroundColor = "#cccccc";    
+                }, 1000);
+                setTimeout(() => {
+                    rowDiv.style.backgroundColor = "#e1e1e1";    
+                }, 1500);
+            });
+
+            // Change the color back to white
+
+            eraseButton.addEventListener("click", () => {
+                rowDiv.style.backgroundColor = "white";
+            })
+        }
     }
 };
 
+// Add colors to the square grid
 
-numbersSquare();
+function numbersSquareColor() {
+    n = slider.value;
+    for (let j = 0; j < n; j++) {
+        const columnDiv = document.createElement("div");
+        columnDiv.classList.add("squareColumn");
+        container.appendChild(columnDiv);
+        for (let i = 0; i < n; i++) {
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("squareRow");
+            columnDiv.appendChild(rowDiv);   
+            
+            // Change the color 
 
-/* let test = document.querySelector("#test");
-let test1 = document.querySelector(".t1");
-let test2 = document.querySelector(".t2"); */
+            rowDiv.addEventListener("mouseover", () => {
+                rowDiv.style.backgroundColor = 'rgb(' + [Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256)].join(',') + ')';
+            });
 
-/* let moreSpace = document.createElement("BR");
-test1.appendChild(moreSpace); */
+            // Change the color back to white
+
+            eraseButton.addEventListener("click", () => {
+                rowDiv.style.backgroundColor = "white";
+            })
+        }
+    }
+};
+
+let slider = document.getElementById("myRange");
+let output = document.getElementById("value");
+
+let button = document.querySelector(".btn");
+let buttonColor = document.querySelector(".btnColor");
+
+button.addEventListener("click", () => {
+    container.innerHTML = "";
+    numbersSquare();
+});
+
+buttonColor.addEventListener("click", () => {
+    container.innerHTML = "";
+    numbersSquareColor();
+});
+
+output.innerHTML = slider.value + " x " + slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value + " x " + this.value;
+}
+
+
+
